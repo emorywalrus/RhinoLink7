8bb289e6c5448eb91e3453ba8f215e042ecd4c76
emorywalrus <72038374+emorywalrus@users.noreply.github.com>
2024-06-20 16:46:42 -0500

    master

--- RhinoLink2/RhinoLink2.h
+++ RhinoLink2/RhinoLink2.h
@@ -7,6 +7,8 @@
 using namespace msclr::interop;
 using namespace RhinoCaller3;
 
+
+// pass window handle and pointer to the QTUI to C#, so Rhino knows what to attach to. I'm sure theres a better way than passing generic pointers.
 __declspec(dllexport) void launch_rhino(void* window_handle, void* ui_ptr) {
 	Caller3::launch_rhino(window_handle, ui_ptr);
 }
